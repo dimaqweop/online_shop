1d5967e4570fd2083b86a13cbe38c3ad71e04d70
dimaqweop <dimaqweop@gmail.com>
2026-08-18 16:35:33 +0300

    home-work-06

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+
+    context = {
+        "title": "Каталог",
+        "products": products
+    }
+
+    return render(request, "main/product_list.html", context)
+
